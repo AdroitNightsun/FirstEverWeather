1517f645691d31e5f68c3b929232ece0cbd11cc7
Karan Kumar <102454582+AdroitNightsun@users.noreply.github.com>
2025-07-16 00:28:08 +0530

    Add API key and URL for weather data; enhance search input with class

---
+++ hot.js
@@ -0,0 +1,4 @@
+const apiKey = "fc27316f32msh02cd8642c5d5eb7p113ebfjsn074c19e900f9";
+const apiUrl = "https://open-weather13.p.rapidapi.com/fivedaysforcast?latitude=40.730610&longitude=-73.935242&lang=EN";
+
+const searchBox = document.querySelector(".search_input");
